f06255323e8d520065279961893d95d6b769ca1a
Mike Pedersen <noctune9@gmail.com>
2014-05-25 22:45:45 +0200

    Improved setup

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
+import shutil
 from distutils.core import setup
+
+shutil.copyfile('so.py', 'so')
 
 setup(name='Stack Overflow',
       version='0.1',
-      description='Stack Overflow browser',
+      description='Text-based Stack Overflow browser',
       author='Mike Pedersen',
       author_email='mipede12@student.aau.dk',
-      scripts=['so']
+      scripts=['so'],
       license='GPL v3'
      )
